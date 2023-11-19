e849bbe13b96921d6ebb059b9ca4cec714758431
Voinov Vladyslav <voinovvladyslav@gmail.com>
2023-11-19 22:16:51 +0200

    feat: scrape categories

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ response = requests.get(BASE_URL)
 print('Status:', response.status_code, BASE_URL)
 
 parser = BooksParser(response.content)
+print(parser.get_categories())
--- scraper/parser.py
+++ scraper/parser.py
@@ -4,3 +4,15 @@ from bs4 import BeautifulSoup
 class BooksParser:
     def __init__(self, page: str | bytes):
         self.soup = BeautifulSoup(page, 'lxml')
+
+    def get_categories(self) -> list[dict]:
+        ul = self.soup.find('ul', 'nav nav-list').find('ul')
+        categories = ul.find_all('a')
+        result = [
+            {
+                'name': x.text.strip(),
+                'url': x.get('href'),
+            }
+            for x in categories
+        ]
+        return result
